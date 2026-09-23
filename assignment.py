85e05a0ed066acf2cadfa2c0e19b9c72bf1edbf9
Alejandro Boyco <alejandro.boyco@pucp.edu.pe>
2026-09-23 17:29:04 +0200

    comments adaded

--- assignment.py
+++ assignment.py
@@ -1,6 +1,16 @@
+
+# this adds the value "tea" to a variable "drink"
 drink = "tea"
+
+# This creates a dictionary with two keys, and two values
 sales = {"tea": 3, "coffee": 5}
+
+# this adds the value from the key "tea" into the "sold" variable
 sold = sales[drink]
+
+# this adds 1 to the previous "sold" value
 sold = sold + 1
+
+# this should print "tea" and "4", respectively
 print(drink)
 print(sold)
